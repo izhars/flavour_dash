c8bc81271ed0faef351ae768ee3483e509915e1d
Sharique Izhar <shariqueizhar@Shariques-MacBook-Air.local>
2024-09-25 19:50:16 +0530

    Your commit message here

--- models/productModel.js
+++ models/productModel.js
@@ -4,47 +4,50 @@ const productSchema = new mongoose.Schema({
 
   id: {
     type: Number,
-    required: true,
+    required: [true, 'Product ID is required'],
   }, 
   name: {
     type: String,
-    required: true,
+    required: [true, 'Product name is required'],
   },
   description: {
     type: String,
-    required: true,
+    required: [true, 'Product description is required'],
   },
   price: {
     type: Number,
-    required: true,
+    required: [true, 'Product price is required'],
   },
   region: {
     type: String,
-    required: true,
+    required: [true, 'Product region is required'],
   },
   weight: {
     type: Number,
-    required: true,
+    required: [true, 'Product weight is required'],
   },
   flavor_profile: {
     type: [String], // Array of strings
-    required: true,
+    required: [true, 'At least one flavor profile is required'],
   },
   grind_option: {
     type: [String], // Array of strings
-    required: true,
+    required: [true, 'At least one grind option is required'],
   },
   roast_level: {
     type: Number,
-    required: true,
-    min: 1,
-    max: 5, // Assuming roast level is between 1 and 5
+    required: [true, 'Roast level is required'],
+    min: [1, 'Roast level cannot be less than 1'],
+    max: [5, 'Roast level cannot be greater than 5'], // Assuming roast level is between 1 and 5
   },
   image_url: {
     type: String,
-    required: true,
+    required: [true, 'Main product image URL is required'],
   },
-  subPics: [String], // Array of URLs for sub-pictures
+  subPics: {
+    type: [String], // Array of URLs for sub-pictures
+    default: [],
+  }
 }, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
